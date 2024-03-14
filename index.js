c9effba0d5dc8ca42dd72a3193080a8ece3a3894
Sarbajit Karmakar <sarbajitkarmakar26@gmail.com>
2024-03-14 17:56:55 +0530

    Set up

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
 
 const app = express();
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/",(req,res)=>{
+    res.render(__dirname+"/views/index.ejs");
+})
 
 app.listen(port,()=>{
     console.log("The website is working");
