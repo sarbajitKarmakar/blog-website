038afd3c8b11f79924317fa7dcc85c2ce5dd9a7c
Sarbajit Karmakar <sarbajitkarmakar26@gmail.com>
2024-03-15 21:42:46 +0530

    starting website procedure

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import { fileURLToPath } from "url";
 
 
 const app = express();
-const port = 3000;
+const port = 80;
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
     res.render(__dirname+"/views/index.ejs");
