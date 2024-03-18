7452e72e9b029e9726e67d20c3cbca8012e8c5b4
Sarbajit Karmakar <sarbajitkarmakar26@gmail.com>
2024-03-18 17:55:24 +0530

    completing html and the css part

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 
 const app = express();
-const port = 80;
+const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static("public"));
