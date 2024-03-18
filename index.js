deff644af6133c94100848b62ef87186e23fe82c
Sarbajit Karmakar <sarbajitkarmakar26@gmail.com>
2024-03-18 20:19:35 +0530

    complete the front end

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-import express from "express";
-import { dirname } from "path";
+import express, { urlencoded } from "express";
+import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
 
 
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
+let obj;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+if (obj) {
+    console.log("true");
+}else{
+    console.log("False");
+}
+
 
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
     res.render(__dirname+"/views/index.ejs");
 })
+
+app.get("/form",(req,res)=>{
+    res.render(__dirname+"/views/form.ejs");
+})
+
+app.post("/submit",(req,res)=>{
+    const newObj = {
+        head: res.body['heading'],
+        comment: res.body['comment']
+    }
+    obj[res.body['name']] = newObj;
+    res.render(__dirname+"/views/index.ejs");
+});
 
 app.listen(port,()=>{
     console.log("The website is working");
---
+++ public/js/script.js
@@ -0,0 +1,27 @@
+const blogs = document.getElementsByClassName('blogs');
+const para = document.getElementsByClassName("para");
+const btn = document.getElementsByClassName("btn")[0];
+
+for (let i = 0; i < blogs.length; i++) {
+    blogs[i].addEventListener("click", (e) => {
+        for (let j = 0; j < blogs.length; j++) {
+            blogs[j].classList.remove("zoom");
+        }
+        if (blogs[i].classList.contains("zoom")) {
+
+            blogs[i].classList.remove("zoom");
+        }else{
+            blogs[i].classList.add("zoom");
+        }
+    });
+}
+
+
+for (let i = 0; i < para.length; i++) {
+    const a = para[i].innerText.slice(0, 690);
+    if (para[i].innerText.length > 690) {
+        para[i].innerHTML = a + `<span class="dot">.....</span>`;
+    }
+
+}
+
